7cf8514a09272df95b10c6e64ad1476d6e081bfc
Maaloks <maaloks91@gmail.com>
2022-07-28 00:16:07 +0300

    div by 3 or 5, but not for 2

---
+++ src/lesson2/Task8.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class Task8 {
+    public static void main(String[] args) {
+        for(int i = 1; i < 101; i++){
+            if(i % 2 != 0){
+                if (i % 5 == 0 || i % 3 == 0) {
+                    System.out.print(i + " ");
+            }
+            }
+        }
+    }
+}
