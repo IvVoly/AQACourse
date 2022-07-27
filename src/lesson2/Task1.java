4b0a7392365d97937846074fa6b2ea8cdd9c7fc3
Maaloks <maaloks91@gmail.com>
2022-07-27 22:38:34 +0300

    add, sub, multi, division

---
+++ src/lesson2/Task1.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 7;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        System.out.println("addition a,b: " + sum);
+        System.out.println("substruction a,b: " + sub);
+        System.out.println("multiplication a,b: " + mul);
+        System.out.println("division a,b: " + div);
+    }
+}
