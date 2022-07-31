d70a118eeed4f374c86ddf3d70447f775e3579b6
Maaloks <maaloks91@gmail.com>
2022-07-31 18:03:27 +0300

    names from a new line.

---
+++ src/lesson3/Task7.java
@@ -0,0 +1,9 @@
+package lesson3;
+
+public class Task7 {
+    public static void main(String[] args) {
+        String names = "Den, John, Will, Kate, Adam, Robin";
+        names = names.replace(", ", "\n");
+        System.out.println(names);
+    }
+}
